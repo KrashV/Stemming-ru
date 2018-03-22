b6ea905f9c5f9d5e1c6ed3360d006043a07ba164
Degranon <boba09@list.ru>
2018-03-22 23:53:31 +0300

    Renamed the folder
    Renamed source file folder
    Fixed exception handling

---
+++ source/Source.cpp
@@ -0,0 +1,30 @@
+#include "StemmerPorter.h"
+#include "FileAnalyser.h"
+
+#include <cstdio>
+#include <windows.h>
+
+#define HELP_USAGE "Usage: <stemmer.exe> <path to source file> <destination .csv file>"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		std::cout << HELP_USAGE;
+		return -1;
+	}
+
+	try {
+		FileAnalyser fa(argv[1]);
+		textmap textmap = fa.stemm();
+		fa.save(argv[1], argv[2], textmap);
+	}
+	catch (std::exception &ex) {
+		std::cout << "Exception occured: " << ex.what() << std::endl;
+		return -1;
+	}
+
+	std::cout << "Finished sucessfully" << std::endl;
+
+	return 0;
+}
